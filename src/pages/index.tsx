cf61f83665a3437cc95d86fc9957ffa9226033d0
weltergab <weltergab@gmail.com>
2023-04-07 15:15:16 -0300

    copia

---
+++ src/pages/index.tsx
@@ -0,0 +1,92 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import Head from 'next/head';
+import { HomeContainer } from '../styles/HomeStyles';
+
+import Header from '../components/Header';
+import HomeHero from '../components/HomeHero';
+import Experiencias from '../components/Experiencias';
+import Projetos from '../components/Projetos';
+import Conhecimentos from '../components/Conhecimentos';
+import FormContato from '../components/FormContato';
+import Footer from '../components/Footer';
+import { getPrismicClient } from '../services/prismic';
+import 'aos/dist/aos.css';
+
+interface IProjeto {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface HomeProps {
+  projetos: IProjeto[];
+}
+
+export default function Home({ projetos }: HomeProps) {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
+  return (
+    <HomeContainer>
+      <Head>
+        <title>Home | Meu portfólio</title>
+        <meta
+          name="description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta
+          property="og:description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+      </Head>
+
+      <Header />
+
+      <main className="container">
+        <HomeHero />
+        <Experiencias />
+        <Projetos projetos={projetos} />
+        <Conhecimentos />
+        <FormContato />
+      </main>
+
+      <Footer />
+    </HomeContainer>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'pro')],
+    { orderings: '[document.first_publication_date desc]' }
+  );
+
+  const projetos = projectResponse.results.map(projeto => ({
+    slug: projeto.uid,
+    title: projeto.data.title,
+    type: projeto.data.type,
+    description: projeto.data.description,
+    link: projeto.data.link.url,
+    thumbnail: projeto.data.thumbnail.url
+  }));
+
+  return {
+    props: {
+      projetos
+    },
+    revalidate: 86400
+  };
+};
